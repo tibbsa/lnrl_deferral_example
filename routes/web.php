fe0974a61304d4334614837aa77e9126b92c9a4b
Anthony Tibbs <anthony@trinimex.ca>
2024-03-29 13:45:53 -0400

    Add testing web URLs to create trial notifications

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/send-notification-to-user', function () {
+    $user = \App\Models\User::firstOrFail();
+    $notification = new \App\Notifications\TestNotification();
+    $user->notify($notification);
+    return response()->json(['success' => true]);
+});
+
+Route::get('/send-notification-to-anonymous', function () {
+    $notification = new \App\Notifications\TestNotification();
+    (new \Illuminate\Notifications\AnonymousNotifiable)->route(
+        'mail',
+        'test2@example.com')
+        ->notify($notification);
+    return response()->json(['success' => true]);
+});
